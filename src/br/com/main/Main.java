d0ab09eeaba02e1c0c8f8af8f36dbe9140166393
gustavoreche <gustavodreche@msn.com>
2020-07-24 16:21:34 -0300

    Altera classe Main, para inserir metodo que escolhe se a forma de extrair o log vai ser local ou externo.

--- src/br/com/main/Main.java
+++ src/br/com/main/Main.java
@@ -1,9 +1,13 @@
 package br.com.main;
 
+import br.com.forma.extrai.informacao.log.FormaDeComoExtraiAInformacaoDoLog;
+import br.com.forma.extrai.informacao.log.FormasDeExtracao;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("ola mundo");
+		System.out.println("Bem vindo leitor de log!");
+		FormasDeExtracao formaDeExtracao = new FormaDeComoExtraiAInformacaoDoLog().pegaForma();
 	}
-
+	
 }
