caeca633ac440dad6974d776f3326209e21f7bc5
gustavoreche <gustavodreche@msn.com>
2020-09-16 23:07:02 -0300

    Cria classe de teste

--- src/main/java/Corrida/br/com/front/TelaInicial.java
+++ src/main/java/Corrida/br/com/front/TelaInicial.java
@@ -2,12 +2,24 @@ package Corrida.br.com.front;
 
 public class TelaInicial {
 	
-	public void exibeOpcoesDeFormaDeExtracaoDoLog() {
+	public void opcoesDeFormaDeExtracaoDoLog() {
 		System.out.println("Escolha a maneira de onde deseja obter as informações do log:");
 		System.out.println("1 - Local");
 		System.out.println("2 - Externo");
 		System.out.println("3 - Encerrar sistema");
 		System.out.print("R: ");
+	}
+
+	public void numeroDeTentativaExcedida() {
+		System.out.println("Número de tentativas excedidas... O sistema será encerrado!");		
+	}
+
+	public void digitouErrado() {
+		System.out.println("Você digitou algo errado. Por favor, tente novamente \n");
+	}
+
+	public void sairDoSistema() {
+		System.out.println("Você escolheu encerrar o sistema. Muito obrigado!!");		
 	}
 
 }
--- src/test/java/Corrida/br/com/front/TelaInicialTeste.java
+++ src/test/java/Corrida/br/com/front/TelaInicialTeste.java
@@ -1,12 +1,35 @@
 package Corrida.br.com.front;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TelaInicialTeste {
 	
+	private static TelaInicial telaInicial;
+	
+	@BeforeClass
+	public static void iniciaAntes() {
+		telaInicial = new TelaInicial();
+	}
+	
 	@Test
 	public void exibeOpcoesDeFormaDeExtracaoDoLog_exibeTexto() {
-		new TelaInicial().exibeOpcoesDeFormaDeExtracaoDoLog();
+		telaInicial.opcoesDeFormaDeExtracaoDoLog();
+	}
+	
+	@Test
+	public void numeroDeTentativaExcedida_exibeTexto() {
+		telaInicial.numeroDeTentativaExcedida();
+	}
+	
+	@Test
+	public void digitouErrado_exibeTexto() {
+		telaInicial.digitouErrado();
+	}
+	
+	@Test
+	public void sairDoSistema_exibeTexto() {
+		telaInicial.sairDoSistema();
 	}
 
 }
