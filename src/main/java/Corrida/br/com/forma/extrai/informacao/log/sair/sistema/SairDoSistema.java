2891f1c14c96c6723916df8cbc936e33aed04ea3
gustavoreche <gustavodreche@msn.com>
2020-09-16 23:06:54 -0300

    Cria classe de teste

--- src/main/java/Corrida/br/com/forma/extrai/informacao/log/sair/sistema/SairDoSistema.java
+++ src/main/java/Corrida/br/com/forma/extrai/informacao/log/sair/sistema/SairDoSistema.java
@@ -3,19 +3,22 @@ package Corrida.br.com.forma.extrai.informacao.log.sair.sistema;
 import Corrida.br.com.forma.extrai.informacao.log.EnumFormasDeExtracao;
 import Corrida.br.com.forma.extrai.informacao.log.FormasDeExtracao;
 import Corrida.br.com.forma.extrai.informacao.log.caractere.digitado.errado.CaractereDigitadoErrado;
+import Corrida.br.com.front.TelaInicial;
 
 public class SairDoSistema implements FormasDeExtracao {
 	
 	private FormasDeExtracao proximaForma;
+	private TelaInicial telaInicial;
 	
 	public SairDoSistema() {
 		this.proximaForma = new CaractereDigitadoErrado();
+		this.telaInicial = new TelaInicial();
 	}
 
 	@Override
 	public Class<? extends FormasDeExtracao> verificaForma(EnumFormasDeExtracao respostaDoUsuario) {
 		if(respostaDoUsuario == EnumFormasDeExtracao.SAIR_DO_SISTEMA) {
-			System.out.println("Você escolheu encerrar o sistema. Muito obrigado!!");
+			this.telaInicial.sairDoSistema();
 			return this.pegaForma();
 		}
 		return this.proximaForma.verificaForma(respostaDoUsuario);
--- src/test/java/Corrida/br/com/forma/extrai/informacao/log/sair/sistema/SairDoSistemaTeste.java
+++ src/test/java/Corrida/br/com/forma/extrai/informacao/log/sair/sistema/SairDoSistemaTeste.java
@@ -1,5 +1,29 @@
 package Corrida.br.com.forma.extrai.informacao.log.sair.sistema;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import Corrida.br.com.forma.extrai.informacao.log.EnumFormasDeExtracao;
+
 public class SairDoSistemaTeste {
+	
+	private static SairDoSistema sairDoSistema;
+	
+	@BeforeClass
+	public static void iniciaAntes() {
+		sairDoSistema = new SairDoSistema();
+	}
+	
+	@Test
+	public void verificaForma() {
+		assertEquals(new SairDoSistema().getClass(), sairDoSistema.verificaForma(EnumFormasDeExtracao.SAIR_DO_SISTEMA));		
+	}
+	
+	@Test
+	public void pegaForma() {
+		assertEquals(new SairDoSistema().getClass(), sairDoSistema.pegaForma());
+	}
 
 }
